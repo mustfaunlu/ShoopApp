78b16336a42ab35b631c9f80741b659445b77698
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-05-21 11:43:31 +0300

    constants modified

--- app/src/main/java/com/mustafaunlu/shoopapp/common/Constants.kt
+++ app/src/main/java/com/mustafaunlu/shoopapp/common/Constants.kt
@@ -6,4 +6,5 @@ object Constants {
     const val SHARED_PREF_CHECKBOX_KEY = "rememberMe"
     const val SHARED_PREF_USERID_KEY = "userId"
     const val SHARED_PREF_DEF = ""
+    const val SHARED_PREF_USERNAME_DEF = "kmeus4"
 }
