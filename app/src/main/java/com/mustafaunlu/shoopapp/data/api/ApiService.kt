e90db3b7bf72368a3fa3ccedbef87fc477409c2f
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-05-21 21:03:39 +0300

    api service strings modified

--- app/src/main/java/com/mustafaunlu/shoopapp/data/api/ApiService.kt
+++ app/src/main/java/com/mustafaunlu/shoopapp/data/api/ApiService.kt
@@ -10,18 +10,28 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("products")
+    @GET(PRODUCTS)
     suspend fun getProducts(): Products
 
-    @GET("products/{id}")
-    suspend fun getProductsById(@Path("id") productId: Int): Product
+    @GET(PRODUCTS_ID)
+    suspend fun getProductsById(@Path(ID) productId: Int): Product
 
-    @POST("auth/login")
+    @POST(AUTH_LOGIN)
     suspend fun login(@Body user: User): UserResponse
 
-    @GET("products/categories")
+    @GET(PRODUCTS_CATEGORIES)
     suspend fun getAllCategories(): List<String>
 
-    @GET("products/category/{categoryName}")
-    suspend fun getProductsByCategory(@Path("categoryName") categoryName: String): Products
+    @GET(PRODUCTS_CATEGORY)
+    suspend fun getProductsByCategory(@Path(CATEGORY_NAME) categoryName: String): Products
+
+    companion object {
+        const val PRODUCTS = "products"
+        const val ID = "id"
+        const val PRODUCTS_ID = "products/{$ID}"
+        const val AUTH_LOGIN = "auth/login"
+        const val PRODUCTS_CATEGORIES = "products/categories"
+        const val CATEGORY_NAME = "categoryName"
+        const val PRODUCTS_CATEGORY = "products/category/{$CATEGORY_NAME}"
+    }
 }
