074cf4ce402d8233a0fe8c498924102b0e92519e
Mustafa ÜNLÜ <mustafaunlu@mail.com>
2023-06-11 15:48:32 +0300

    single ui data mapper added

--- app/src/main/java/com/mustafaunlu/shoopapp/ui/shoppingList/CartUiMapper.kt
+++ app/src/main/java/com/mustafaunlu/shoopapp/ui/shoppingList/CartUiMapper.kt
@@ -2,6 +2,7 @@ package com.mustafaunlu.shoopapp.ui.shoppingList
 
 import com.mustafaunlu.shoopapp.common.UserCartUiData
 import com.mustafaunlu.shoopapp.domain.entity.UserCartEntity
+import com.mustafaunlu.shoopapp.domain.mapper.ProductBaseMapper
 import com.mustafaunlu.shoopapp.domain.mapper.ProductListMapper
 import javax.inject.Inject
 
@@ -16,5 +17,18 @@ class CartUiMapper @Inject constructor() : ProductListMapper<UserCartEntity, Use
                 imageUrl = it.image,
             )
         }
+    }
+}
+
+class SingleCartUiMapper @Inject constructor() : ProductBaseMapper<UserCartUiData, UserCartEntity> {
+    override fun map(input: UserCartUiData): UserCartEntity {
+        return UserCartEntity(
+            userId = 0,
+            productId = input.id,
+            price = input.price,
+            quantity = input.quantity,
+            title = input.title,
+            image = input.imageUrl,
+        )
     }
 }
